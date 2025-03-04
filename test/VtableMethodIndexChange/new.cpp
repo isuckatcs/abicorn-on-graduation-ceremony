5ca9649ea8ba3d0b436d9c43b9514de67aa58bb0
isuckatcs <65320245+isuckatcs@users.noreply.github.com>
2025-03-04 18:09:33 +0100

    [VTableChecker] test if there are non-virtual methods between virtual ones

--- test/VtableMethodIndexChange/new.cpp
+++ test/VtableMethodIndexChange/new.cpp
@@ -1,21 +1,22 @@
 class c{
 virtual void bar();
+void nonVirtual();
 virtual void foo();
 };
 
-// CHECK: new.cpp:3:14: warning: change in virtual function index [vtable-checker]
+// CHECK: new.cpp:4:14: warning: change in virtual function index [vtable-checker]
 // CHECK-NEXT: virtual void foo();
 // CHECK-NEXT:              ^
 // CHECK-NEXT: old.cpp:2:14: note: virtual function 'foo' is at index 0 in the old library [vtable-checker]
 // CHECK-NEXT: virtual void foo();
 // CHECK-NEXT:              ^
-// CHECK-NEXT: new.cpp:3:14: note: virtual function 'foo' is at index 1 in the new library [vtable-checker]
+// CHECK-NEXT: new.cpp:4:14: note: virtual function 'foo' is at index 1 in the new library [vtable-checker]
 // CHECK-NEXT: virtual void foo();
 // CHECK-NEXT:              ^
 // CHECK-NEXT: new.cpp:2:14: warning: change in virtual function index [vtable-checker]
 // CHECK-NEXT: virtual void bar();
 // CHECK-NEXT:              ^
-// CHECK-NEXT: old.cpp:3:14: note: virtual function 'bar' is at index 1 in the old library [vtable-checker]
+// CHECK-NEXT: old.cpp:4:14: note: virtual function 'bar' is at index 1 in the old library [vtable-checker]
 // CHECK-NEXT: virtual void bar();
 // CHECK-NEXT:              ^
 // CHECK-NEXT: new.cpp:2:14: note: virtual function 'bar' is at index 0 in the new library [vtable-checker]
--- test/VtableMethodIndexChange/old.cpp
+++ test/VtableMethodIndexChange/old.cpp
@@ -1,4 +1,5 @@
 class c{
 virtual void foo();
+void nonVirtual();
 virtual void bar();
 };
